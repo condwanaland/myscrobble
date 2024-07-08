dcacff1089a2cfd745e2d1aa5bac6e2b159a2188
Conor Neilson <condwanaland@gmail.com>
2024-07-08 19:02:33 +1200

    stop writing new rda files everytime

--- R/run.R
+++ R/run.R
@@ -2,11 +2,6 @@ library(scrobbler)
 
 mydf <- download_scrobbles()
 
-saveRDS(mydf,
-        file = paste0(
-          "scrobbles/scrobbles-",
-          make.names(Sys.Date()),
-          ".Rda")
-)
+saveRDS(mydf, file = "scrobbles/scrobbles.Rda")
 
 write.csv(mydf, "scrobbles/scrobbles.csv", row.names = FALSE)
