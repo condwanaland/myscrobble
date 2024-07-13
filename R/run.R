d69447ac357591a7c89a2a09e4bc219e94cd8b66
Conor Neilson <condwanaland@gmail.com>
2024-07-13 20:27:30 +1200

    move to incremental

--- R/run.R
+++ R/run.R
@@ -1,6 +1,12 @@
 library(scrobbler)
 
-mydf <- download_scrobbles()
+#mydf <- download_scrobbles()
+
+url <- "https://raw.githubusercontent.com/condwanaland/myscrobble/main/scrobbles/scrobbles.csv"
+
+data <- read.csv(url)
+
+mydf <- update_scrobbles(data, 'date_unix')
 
 saveRDS(mydf, file = "scrobbles/scrobbles.Rda")
 
